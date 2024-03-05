147631db6d364ac497f6a75aeb143d98d288e6f0
anoopraju31 <anoopraju31@gmail.com>
2024-03-05 13:27:19 +0530

    MainContainer intial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Breadcrumb from './components/Breadcrumb'
+import MainContainer from './components/MainContainer'
 
 const Home = () => {
 	return (
 		<div className='h-[300vh] p-4 lg:px-8'>
 			<Breadcrumb path={['Cryptocurrencies', 'Bitcoin']} />
+			<main className='flex gap-5'>
+				<MainContainer />
+			</main>
 		</div>
 	)
 }
