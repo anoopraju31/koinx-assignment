f9dbf8bcacdb7550a7e28360cc86c639dddcd2e3
anoopraju31 <anoopraju31@gmail.com>
2024-03-05 16:02:29 +0530

    About section - initial

--- src/app/components/MainContainer.tsx
+++ src/app/components/MainContainer.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import EventCard from './EventCard'
 import EventCarousel from './EventCarousel'
+import About from '../(currencies)/sections/About'
 
 const MainContainer = () => {
 	return (
 		<div className='col-span-full lg:col-span-9 px-4'>
 			<div className='w-full my-4'>
 				<EventCarousel />
+				<About />
 			</div>
 		</div>
 	)
