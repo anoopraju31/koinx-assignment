49306e6d4b2188cc6a695056a74cd9f55c9c2a72
anoopraju31 <anoopraju31@gmail.com>
2024-03-05 21:43:03 +0530

    Performance Section - initial

---
+++ src/app/(currencies)/sections/Performance.tsx
@@ -0,0 +1,12 @@
+import Tabs from '@/app/components/Tabs'
+import React from 'react'
+
+const Performance = () => {
+	return (
+		<div>
+			<Tabs />
+		</div>
+	)
+}
+
+export default Performance
--- src/app/components/MainContainer.tsx
+++ src/app/components/MainContainer.tsx
@@ -4,14 +4,14 @@ import About from '../(currencies)/sections/About'
 import AlreadyHoldingCrypto from '../(currencies)/sections/AlreadyHoldingCrypto'
 import Tokenomics from '../(currencies)/sections/Tokenomics'
 import Team from '../(currencies)/sections/Team'
-import Tabs from '../components/Tabs'
+import Performance from '../(currencies)/sections/Performance'
 
 const MainContainer = () => {
 	return (
 		<div className='col-span-full lg:col-span-9 sm:px-4'>
 			<div className='w-full my-4'>
 				<Hero />
-				<Tabs />
+				<Performance />
 				<Sentiment />
 				<About />
 				<AlreadyHoldingCrypto />
