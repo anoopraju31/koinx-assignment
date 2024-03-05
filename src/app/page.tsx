753d4aad5a3aa6e5d13f54d1f84419760380a9d7
anoopraju31 <anoopraju31@gmail.com>
2024-03-05 13:24:55 +0530

    Breadcrumbs added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Breadcrumb from './components/Breadcrumb'
 
 const Home = () => {
-	return <div className='h-[300vh]'>Home</div>
+	return (
+		<div className='h-[300vh] p-4 lg:px-8'>
+			<Breadcrumb path={['Cryptocurrencies', 'Bitcoin']} />
+		</div>
+	)
 }
 
 export default Home
