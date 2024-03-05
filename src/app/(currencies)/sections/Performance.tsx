05455967274636fec4c06f279a00b4792cbaef1a
anoopraju31 <anoopraju31@gmail.com>
2024-03-05 22:13:20 +0530

    Performance Section - Range slider added

--- src/app/(currencies)/sections/Performance.tsx
+++ src/app/(currencies)/sections/Performance.tsx
@@ -1,3 +1,4 @@
+import SectionHeading from '@/app/components/SectionHeading'
 import Tabs from '@/app/components/Tabs'
 import React from 'react'
 
@@ -5,8 +6,61 @@ const Performance = () => {
 	return (
 		<div>
 			<Tabs />
+
+			<section className='md:p-6'>
+				<SectionHeading heading='Performance' />
+
+				<div className='py-5 flex flex-col gap-7'>
+					<RangeSlider
+						titleLeft="Today's Low"
+						titleRight="Today's High"
+						valueLeft='46,930.22'
+						valueRight='49,343.83'
+					/>
+					<RangeSlider
+						titleLeft='52W Low'
+						titleRight='52W High'
+						valueLeft='16,930.22'
+						valueRight='49,743.83'
+					/>
+				</div>
+			</section>
 		</div>
 	)
 }
 
 export default Performance
+
+interface RangeSliderProps {
+	titleLeft: string
+	valueLeft: string
+	titleRight: string
+	valueRight: string
+}
+
+const RangeSlider: React.FC<RangeSliderProps> = ({
+	titleLeft,
+	titleRight,
+	valueLeft,
+	valueRight,
+}) => {
+	return (
+		<div className='flex items-center gap-7'>
+			<div className='flex flex-col gap-[10px] w-28'>
+				<p className='text-sm leading-5 text-[#44475b]'> {titleLeft} </p>
+				<p className='text-base leading-6 text-[#44475b] font-medium'>
+					{valueLeft}
+				</p>
+			</div>
+
+			<div className='flex-1 h-2 w-full bg-rainbow-gradient rounded-lg'></div>
+
+			<div className='flex flex-col gap-[10px] w-28 text-right'>
+				<p className='text-sm leading-5 text-[#44475b]'> {titleRight} </p>
+				<p className='text-base leading-6 text-[#44475b] font-medium'>
+					{valueRight}
+				</p>
+			</div>
+		</div>
+	)
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,8 @@ const config: Config = {
 			'green-gradient': 'linear-gradient(135deg, #79F1A4 0%, #0E5CAD 100%)',
 			'orange-gradient':
 				'linear-gradient(135.73deg, #FF9865 0%, #EF3031 110.55%)',
+			'rainbow-gradient':
+				'linear-gradient(90deg, #FF4949 0%, #FF4E11 15.34%, #FC870A 30.45%, #FFAF11 48.68%, #C2CB21 62.75%, #11EB68 100.03%)',
 		},
 		boxShadow: {
 			nav: '0px 0px 12px 0 #1026490F',
