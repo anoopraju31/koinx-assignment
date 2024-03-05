316dfa104a13349aca87521550be84e304c3c259
anoopraju31 <anoopraju31@gmail.com>
2024-03-05 16:07:46 +0530

    section heading - styled

--- src/app/(currencies)/sections/About.tsx
+++ src/app/(currencies)/sections/About.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const About = () => {
 	return (
-		<section>
+		<section className='p-4'>
 			<SectionHeading heading='About Bitcoin' />
 		</section>
 	)
--- src/app/components/SectionHeading.tsx
+++ src/app/components/SectionHeading.tsx
@@ -5,7 +5,9 @@ interface SectionHeadingProps {
 }
 
 const SectionHeading: React.FC<SectionHeadingProps> = ({ heading }) => {
-	return <h2 className='font-semibold text-2xl text-[#0f1629]'> {heading} </h2>
+	return (
+		<h2 className='font-semibold text-2xl text-[#0f1629] mb-5'> {heading} </h2>
+	)
 }
 
 export default SectionHeading
