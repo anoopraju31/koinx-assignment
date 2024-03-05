d236f701a9b8b67ae3bd7b9dc5b3c660f0a795bb
anoopraju31 <anoopraju31@gmail.com>
2024-03-05 19:28:26 +0530

    Secondary section heading - styled

--- src/app/(currencies)/sections/Sentiment.tsx
+++ src/app/(currencies)/sections/Sentiment.tsx
@@ -1,7 +1,14 @@
+import SecondarySectionHeading from '@/app/components/SecondarySectionHeading'
+import SectionHeading from '@/app/components/SectionHeading'
 import React from 'react'
 
 const Sentiment = () => {
-	return <div>Sentiment</div>
+	return (
+		<div>
+			<SectionHeading heading='Sentiment' />
+			<SecondarySectionHeading />
+		</div>
+	)
 }
 
 export default Sentiment
--- src/app/components/SecondarySectionHeading.tsx
+++ src/app/components/SecondarySectionHeading.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { IoMdInformationCircle } from 'react-icons/io'
 
 const SecondarySectionHeading = () => {
-	return <div>SecondarySectionHeading</div>
+	return (
+		<div className='flex items-center gap-2'>
+			<h3 className='text-lg font-semibold text-[#44475b]'>Key Events</h3>
+			<div className='text-[#7C7E8C] text-xl'>
+				<IoMdInformationCircle size={24} />
+			</div>
+		</div>
+	)
 }
 
 export default SecondarySectionHeading
