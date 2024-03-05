d451518a0c3dda52f591c5942c1559fd9bfe171c
anoopraju31 <anoopraju31@gmail.com>
2024-03-05 14:21:56 +0530

    Event Card - initial

--- src/app/components/MainContainer.tsx
+++ src/app/components/MainContainer.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import EventCard from './EventCard'
 
 const MainContainer = () => {
-	return <div>MainContainer</div>
+	return (
+		<div className='flex-1 w-full bg-green-500 px-4'>
+			<EventCard />
+		</div>
+	)
 }
 
 export default MainContainer
