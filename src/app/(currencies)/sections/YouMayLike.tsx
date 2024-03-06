e3a5ecd77159e68be3f69df6b45548b327b8be51
anoopraju31 <anoopraju31@gmail.com>
2024-03-06 15:17:28 +0530

    Coin Caroousel - initial

--- src/app/(currencies)/sections/YouMayLike.tsx
+++ src/app/(currencies)/sections/YouMayLike.tsx
@@ -1,7 +1,14 @@
+import CoinCarousel from '@/app/components/CoinCarousel'
+import SectionHeading from '@/app/components/SectionHeading'
 import React from 'react'
 
 const YouMayLike = () => {
-	return <div>YouMayLike</div>
+	return (
+		<div>
+			<SectionHeading heading='You May Also Like' />
+			<CoinCarousel />
+		</div>
+	)
 }
 
 export default YouMayLike
---
+++ src/app/components/CoinCarousel.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const CoinCarousel = () => {
+	return <div>CoinCarousel</div>
+}
+
+export default CoinCarousel
